e1d85d70749d7d115edeabc7de9f8d32ae330755
yoshitofurukawa <fullkawas@gmail.com>
2024-11-02 21:48:40 +0900

    step7新規登録で画像ファイルのエラー発生中

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(CompaniesTableSeeder::class);
+        $this->call(SalesTableSeeder::class);
+        // 他のシーダーも呼び出す場合は、ここに追加します。
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// "Route"というツールを使うために必要な部品を取り込んでいます。
+use App\Http\Controllers\ProductController;
+// ProductControllerに繋げるために取り込んでいます
+use Illuminate\Support\Facades\Auth;
+// "Auth"という部品を使うために取り込んでいます。この部品はユーザー認証（ログイン）に関する処理を行います
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+// 商品一覧画面
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+// 商品登録フォーム画面
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+
+// 商品追加画面
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+// 商品詳細画面
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+// 商品更新フォーム画面
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+
+// 商品更新
+Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+// 商品削除
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('products', ProductController::class);
 });
+
